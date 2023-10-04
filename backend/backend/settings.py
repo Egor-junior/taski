c241125e522c77679bf247fd23f164c9df481476
Egor <yegor.belov.1995@internet.ru>
2023-10-04 06:25:54 +0000

    reba

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -7,7 +7,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = False
 
-ALLOWED_HOSTS = ['158.160.79.3', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['158.160.79.3', '127.0.0.1', 'localhost', 'taskiorenproject.ddns.net']
 
 
 # Application definition
@@ -103,7 +103,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = 'static_backend'
+STATIC_URL = 'static_backend/'
 STATIC_ROOT = BASE_DIR / 'static_backend'
 
 # Default primary key field type
